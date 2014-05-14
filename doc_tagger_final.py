3d8152877ab1246483841cff0bba52a8f7268d82
lucysun <lucy.a.sun@gmail.com>
2014-05-14 15:47:45 -0400

    Update doc_tagger_final.py

--- doc_tagger_final.py
+++ doc_tagger_final.py
@@ -19,9 +19,28 @@ def open_file(file_path):
 		return f.read()
 
 def kw_compiler(kws):
-"""Given user-supplied keywords, create regular expressions for searching for them."""
+"""Given user-supplied keywords, creates regular expressions for searching for them."""
 	for kw in kws:
 		return re.compile(r'\b' + kw + r'\b', re.IGNORECASE)
+		
+def kw_counter(kw_searches, text)
+"""For each keyword, counts the number of occurrences in the text."""
+	for kw_search in kw_searches:
+		return len(re.findall(kw_searches, text))
+
+title_search = re.compile(r'title:(?P<title>.*)', re.IGNORECASE)
+author_search = re.compile(r'author:(?P<author>.*)', re.IGNORECASE)
+translator_search = re.compile(r'translator:(?P<translator>.*)', re.IGNORECASE)
+illustrator_search = re.compile(r'illustrator:(?P<illustrator>.*)', re.IGNORECASE)
+metadata_dict = (title = title_search, author = author_search, translator = translator_search, illustrator = illustrator_search)
+
+def metadata_searcher(metadata_dict, text)
+	for m in metadata_dict:
+		results = {}
+		result = re.search(metadata_dict[m], text)
+		if result:
+			results[m] = result.group(m)
+		return results
 
 def print_results(directory, kws):
 	for fl in os.pathdir(directory):
@@ -29,6 +48,13 @@ def print_results(directory, kws):
 			file_path = make_file_path(directory, fl)
 			text = open_file(file_path)
 			print "Here's the info for file {}.".format(fl)
-			
+			kw_searches = kw_compiler(kws)
+			metadata_searches = metadata_searcher(metadata_dict, text)
+			for m in metadata_searches:
+				print "{0}: {1}.".format(m, metadata_searches[m])
+			for kw in kw_searches:
+				kw_count = kw_counter(kw_searches, text)
+				print "The word {0} appears {1} times.".format(kw, kw_counter(kw_count, text))
+		print "*" *25	
 			
 			
