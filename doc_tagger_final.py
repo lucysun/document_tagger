f280649ef980a8e9f66c2d2a6f29909690f0cfc6
lucysun <lucy.a.sun@gmail.com>
2014-05-14 15:52:28 -0400

    Update doc_tagger_final.py

--- doc_tagger_final.py
+++ doc_tagger_final.py
@@ -17,6 +17,21 @@ def open_file(file_path):
 """Given a file path, opens the file and returns its contents."""
 	with open(file_path, "r+") as f:
 		return f.read()
+		
+title_search = re.compile(r'title:(?P<title>.*)', re.IGNORECASE)
+author_search = re.compile(r'author:(?P<author>.*)', re.IGNORECASE)
+translator_search = re.compile(r'translator:(?P<translator>.*)', re.IGNORECASE)
+illustrator_search = re.compile(r'illustrator:(?P<illustrator>.*)', re.IGNORECASE)
+metadata_dict = (title = title_search, author = author_search, translator = translator_search, illustrator = illustrator_search)
+
+def metadata_searcher(metadata_dict, text):
+"""Searches for metadata information (title, author, translator, illustrator) and returns results."""
+	for m in metadata_dict:
+		results = {}
+		result = re.search(metadata_dict[m], text)
+		if result:
+			results[m] = result.group(m)
+		return results
 
 def kw_compiler(kws):
 """Given user-supplied keywords, creates regular expressions for searching for them."""
@@ -28,33 +43,28 @@ def kw_counter(kw_searches, text)
 	for kw_search in kw_searches:
 		return len(re.findall(kw_searches, text))
 
-title_search = re.compile(r'title:(?P<title>.*)', re.IGNORECASE)
-author_search = re.compile(r'author:(?P<author>.*)', re.IGNORECASE)
-translator_search = re.compile(r'translator:(?P<translator>.*)', re.IGNORECASE)
-illustrator_search = re.compile(r'illustrator:(?P<illustrator>.*)', re.IGNORECASE)
-metadata_dict = (title = title_search, author = author_search, translator = translator_search, illustrator = illustrator_search)
-
-def metadata_searcher(metadata_dict, text)
-	for m in metadata_dict:
-		results = {}
-		result = re.search(metadata_dict[m], text)
-		if result:
-			results[m] = result.group(m)
-		return results
-
 def print_results(directory, kws):
+"""Print results of metadata and keyword searches for each file."""
 	for fl in os.pathdir(directory):
 		if fl.endswith('.txt'):
 			file_path = make_file_path(directory, fl)
 			text = open_file(file_path)
 			print "Here's the info for file {}.".format(fl)
-			kw_searches = kw_compiler(kws)
 			metadata_searches = metadata_searcher(metadata_dict, text)
 			for m in metadata_searches:
 				print "{0}: {1}.".format(m, metadata_searches[m])
+			kw_searches = kw_compiler(kws)
 			for kw in kw_searches:
 				kw_count = kw_counter(kw_searches, text)
 				print "The word {0} appears {1} times.".format(kw, kw_counter(kw_count, text))
-		print "*" *25	
+		print "*" *25		
+
+def main(directory, kws):
+	directory = sys.argv[1]
+	kws = sys.argv[2:]
+	print_results(directory, kws)
+
+if __name__ = '__main__'
+			
 			
 			
